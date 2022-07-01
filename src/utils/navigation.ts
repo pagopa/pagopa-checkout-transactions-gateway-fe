932fa7133f2f21c3e43699c69bc6546f93a2a8df
Nazar Chekalin <nazcheck@gmail.com>
2022-07-01 15:56:35 +0200

    Edit request id with anchor

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -2,8 +2,11 @@
 export function getRequestId() {
   return window.location.href.includes("?urlRedirect=")
     ? // eslint-disable-next-line functional/immutable-data
-      window.location.href.split("?urlRedirect=")[0].split("/").pop()
-    : window.location.href.split("/").pop();
+      window.location.href
+        .split("?urlRedirect=")[0]
+        .split("#")
+        .pop()
+    : window.location.href.split("#").pop();
 }
 
 export function getCurrentLocation() {

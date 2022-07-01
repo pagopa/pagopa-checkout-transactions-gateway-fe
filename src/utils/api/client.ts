b932d6ef20287815cd06fef09c02cb333fb60cf6
Simone Infante <simone.infante@pagopa.it>
2022-07-01 10:37:35 +0200

    fix: disable unused fetch client

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -1,23 +1,23 @@
-import { agent } from "@pagopa/ts-commons";
-import {
-  AbortableFetch,
-  setFetchTimeout,
-  toFetch
-} from "@pagopa/ts-commons/lib/fetch";
-import { Millisecond } from "@pagopa/ts-commons/lib/units";
-import { createClient } from "../../../generated/definitions/gateway-transactions-api/client";
-import { getConfig } from "../config";
+// import { agent } from "@pagopa/ts-commons";
+// import {
+//   AbortableFetch,
+//   setFetchTimeout,
+//   toFetch
+// } from "@pagopa/ts-commons/lib/fetch";
+// import { Millisecond } from "@pagopa/ts-commons/lib/units";
+// import { createClient } from "../../../generated/definitions/gateway-transactions-api/client";
+// import { getConfig } from "../config";
 
-const abortableFetch = AbortableFetch(agent.getHttpFetch(process.env));
-const fetchWithTimeout = toFetch(
-  setFetchTimeout(getConfig().API_TIMEOUT as Millisecond, abortableFetch)
-);
-// tslint:disable-next-line: no-any
-const fetchApi: typeof fetchWithTimeout =
-  fetch as any as typeof fetchWithTimeout;
+// const abortableFetch = AbortableFetch(agent.getHttpFetch(process.env));
+// const fetchWithTimeout = toFetch(
+//   setFetchTimeout(getConfig().API_TIMEOUT as Millisecond, abortableFetch)
+// );
+// // tslint:disable-next-line: no-any
+// const fetchApi: typeof fetchWithTimeout =
+//   fetch as any as typeof fetchWithTimeout;
 
-export const apiTransactionsClient = createClient({
-  baseUrl: getConfig().API_HOST,
-  basePath: getConfig().API_BASEPATH,
-  fetchApi
-});
+// export const apiTransactionsClient = createClient({
+//   baseUrl: getConfig().API_HOST,
+//   basePath: getConfig().API_BASEPATH,
+//   fetchApi
+// });

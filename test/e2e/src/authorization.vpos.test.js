164ebf905da57477372f2f27b262172323b625a5
Pietro Tota <pietro.tota@pagopa.it>
2023-01-24 17:14:16 +0100

    feat: add vpos direct auth tests

--- test/e2e/src/authorization.vpos.test.js
+++ test/e2e/src/authorization.vpos.test.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 describe('Transaction gateway FE VPOS authorization tests', () => {
   /**
    * Test input and configuration
@@ -19,6 +21,10 @@ describe('Transaction gateway FE VPOS authorization tests', () => {
    */
   const VPOS_AUTH_URL = process.env.VPOS_AUTH_URL;
 
+  const VPOS_MOCK_API_KEY = process.env.VPOS_MOCK_API_KEY;
+
+  const VPOS_API_KEY = process.env.VPOS_API_KEY;
+
   /**
    * Increase default test timeout (5000ms)
    * to support entire payment flow
@@ -30,22 +36,28 @@ describe('Transaction gateway FE VPOS authorization tests', () => {
   });
 
   it('VPOS - Should complete step 0 direct authorization', async () => {
-    let requestId;
-    if (VPOS_USE_PGS_MOCK == true) {
+    if (VPOS_USE_PGS_MOCK === "true") {
       //first call mock api for configure direct authorization
-      configureMockStep0DirectAuth("00","00","00").catch(error => {throw error});
-      requestId = authRequestVpos().then(response.body.requestId).catch(error => {throw error});
-    }else{
-      requestId = process.env.VPOS_STEP_0_DIRECT_AUTH_REQUEST_ID;
+      expect(VPOS_MOCK_API_KEY).toBe(200);
+      expect(configureMockStep0DirectAuth(VPOS_MOCK_CONFIGURATION_URL, "00", "00", "00", VPOS_MOCK_API_KEY)).resolves.toBe(200);
+      let response = await authRequestVpos(VPOS_AUTH_URL, VPOS_API_KEY);
+      expect(response).not.toBe("");
+      await auth0Test(response.requestId, response.returnUrl)
+    } else {
+      await auth0Test(process.env.VPOS_STEP_0_DIRECT_AUTH_REQUEST_ID, 'www.google.com')
     }
-      await page.goto(`${PAYMENT_TRANSACTION_GATEWAY_FE_URL}/vpos/${requestId}`);
-      await page.waitForNavigation();
-      const finalUrl = page.url();
-      expect(finalUrl).toContain('www.google.com');
-    });
+
+  });
 });
 
-const configureMockStep0DirectAuth = (step0Outcome, step1Outcome,step2Outcome) => fetch(VPOS_MOCK_CONFIGURATION_URL, {
+const auth0Test = async (requestId, returnUrl) => {
+  await page.goto(`${PAYMENT_TRANSACTION_GATEWAY_FE_URL}/vpos/${requestId}`);
+  await page.waitForNavigation();
+  const finalUrl = page.url();
+  expect(finalUrl).toContain(returnUrl);
+}
+
+const configureMockStep0DirectAuth = async (url, step0Outcome, step1Outcome, step2Outcome, apiKey) => fetch(url, {
   method: "POST",
   body: JSON.stringify({
     "method3dsOutcome": "OK",
@@ -57,19 +69,15 @@ const configureMockStep0DirectAuth = (step0Outcome, step1Outcome,step2Outcome) =
     "httpOutcome": "200"
   }),
   headers: {
-    "Content-type": "application/json; charset=UTF-8"
+    "Content-type": "application/json; charset=UTF-8",
+    "ocp-apim-subscription-key": apiKey
   }
-}).then((resp) => {
-  if (resp.ok) {
-    return resp.json();
-  }
-  throw new Error("Error configuring mock");
-});
+}).then(response => response.status);
 
-const authRequestVpos = () => fetch(VPOS_AUTH_URL, {
+const authRequestVpos = async (url, apiKey) => fetch(url, {
   method: "POST",
   body: JSON.stringify({
-    "idTransaction": "{{$guid}}",
+    "idTransaction": "{{$guid}}", //TODO Cambiare id
     "reqRefNumber": "123456",
     "amount": 12345,
     "pan": "123456789124",
@@ -81,13 +89,15 @@ const authRequestVpos = () => fetch(VPOS_AUTH_URL, {
     "emailCH": "test@mail.com",
     "isFirstPayment": false,
     "idPsp": "BIC36019"
-}),
+  }),
   headers: {
-    "Content-type": "application/json; charset=UTF-8"
+    "Content-type": "application/json; charset=UTF-8",
+    "ocp-apim-subscription-key": apiKey
   }
-}).then((resp) => {
-  if (resp.ok) {
-    return resp.json();
+}).then(response => {
+  if (response.ok) {
+    return response.json()
+  } else {
+    return "";
   }
-  throw new Error("Error performing VPOS authorization");
 });

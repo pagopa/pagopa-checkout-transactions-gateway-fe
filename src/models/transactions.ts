73e3a31983d07158d74f2b8cd65e326799d1ae93
MarcoAGreco <marcoandreagreco@gmail.com>
2023-02-20 15:31:20 +0100

    [CHK-1121] fix: add final redirect after completed authorization

--- src/models/transactions.ts
+++ src/models/transactions.ts
@@ -2,11 +2,3 @@ export enum Channel {
   WEB = "WEB",
   APP = "APP"
 }
-
-export interface XPayResponse {
-  html: string;
-  status: string;
-  authOutcome: string;
-  authCode: string;
-  redirectUrl: string;
-}

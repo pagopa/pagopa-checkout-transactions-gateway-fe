52c2bc096ca15fb456f76d8ab615b1af4a7557f0
Simone Infante <simone.infante@pagopa.it>
2023-07-10 19:26:20 +0200

    fix: format

--- test/e2e/src/utils/vposUtils.js
+++ test/e2e/src/utils/vposUtils.js
@@ -1,12 +1,12 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 import { v4 as uuidv4 } from 'uuid';
 /**
-   * VPOS mock configuration URL 
-   */
+ * VPOS mock configuration URL
+ */
 const VPOS_MOCK_CONFIGURATION_URL = process.env.VPOS_MOCK_CONFIGURATION_URL;
 
 /**
- * VPOS authorization 
+ * VPOS authorization
  */
 const VPOS_AUTH_URL = process.env.VPOS_AUTH_URL;
 
@@ -15,70 +15,72 @@ const VPOS_MOCK_API_KEY = process.env.VPOS_MOCK_API_KEY;
 const VPOS_API_KEY = process.env.VPOS_API_KEY;
 
 export const configurePgsMockDev = async (step0Outcome, step1Outcome, step2Outcome) => {
-    let authRequestId = "";
-    console.log(`Configuring PGS mock for test and performing auth request:
+  let authRequestId = '';
+  console.log(`Configuring PGS mock for test and performing auth request:
     step0Outcome: [${step0Outcome}]
     step1Outcome: [${step1Outcome}]
     step2Outcome: [${step2Outcome}]`);
-    let mockConfigurationStatusCode = await configureMockStepDirectAuth(step0Outcome, step1Outcome, step2Outcome);
-    console.log(`PGS mock configuration http response status code: [${mockConfigurationStatusCode}]`);
-    if (mockConfigurationStatusCode === 200) {
-      let response = await authRequestVpos();
-      if(response.errorHttpStatus===undefined){
-        authRequestId = response.requestId;
-      }else{
-        console.log(`ERROR: KO auth request response received. http status code: [${response.errorHttpStatus}]`);
-      }
-    }else{
-      console.log('ERROR: KO PGS mock configuration response received.');
+  const mockConfigurationStatusCode = await configureMockStepDirectAuth(step0Outcome, step1Outcome, step2Outcome);
+  console.log(`PGS mock configuration http response status code: [${mockConfigurationStatusCode}]`);
+  if (mockConfigurationStatusCode === 200) {
+    const response = await authRequestVpos();
+    if (response.errorHttpStatus === undefined) {
+      authRequestId = response.paymentAuthorizationId;
+    } else {
+      console.log(`ERROR: KO auth request response received. http status code: [${response.errorHttpStatus}]`);
     }
-    console.log(`DEV auth request received: [${authRequestId}]`);
-    return authRequestId;
+  } else {
+    console.log('ERROR: KO PGS mock configuration response received.');
   }
-  
-  export const configureMockStepDirectAuth = async (step0Outcome, step1Outcome, step2Outcome) => fetch(VPOS_MOCK_CONFIGURATION_URL, {
-    method: "POST",
+  console.log(`DEV auth request received: [${authRequestId}]`);
+  return authRequestId;
+};
+
+export const configureMockStepDirectAuth = async (step0Outcome, step1Outcome, step2Outcome) =>
+  fetch(VPOS_MOCK_CONFIGURATION_URL, {
+    method: 'POST',
     body: JSON.stringify({
-      "method3dsOutcome": "OK",
-      "step0Outcome": step0Outcome,
-      "step1Outcome": step1Outcome,
-      "step2Outcome": step2Outcome,
-      "transactionStatusOutcome": "00",
-      "orderStatusOutcome": "00",
-      "httpOutcome": "200"
+      method3dsOutcome: 'OK',
+      step0Outcome,
+      step1Outcome,
+      step2Outcome,
+      transactionStatusOutcome: '00',
+      orderStatusOutcome: '00',
+      httpOutcome: '200',
     }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "ocp-apim-subscription-key": VPOS_MOCK_API_KEY
-    }
+      'Content-type': 'application/json; charset=UTF-8',
+      'ocp-apim-subscription-key': VPOS_MOCK_API_KEY,
+    },
   }).then(response => response.status);
-  
-  export const authRequestVpos = async () => fetch(VPOS_AUTH_URL, {
-    method: "POST",
+
+export const authRequestVpos = async () =>
+  fetch(VPOS_AUTH_URL, {
+    method: 'POST',
     body: JSON.stringify({
-      "idTransaction": uuidv4(),
-      "reqRefNumber": "123456",
-      "amount": 12345,
-      "pan": "123456789124",
-      "securityCode": "123",
-      "expireDate": "30/12",
-      "holder": "Username Surname",
-      "circuit": "MASTERCARD",
-      "threeDsData": "threeDSData",
-      "emailCH": "test@mail.com",
-      "isFirstPayment": false,
-      "idPsp": "BIC36019"
+      idTransaction: uuidv4(),
+      reqRefNumber: '123456',
+      amount: 12345,
+      pan: '123456789124',
+      securityCode: '123',
+      expireDate: '30/12',
+      holder: 'Username Surname',
+      circuit: 'MASTERCARD',
+      threeDsData: 'threeDSData',
+      emailCH: 'test@mail.com',
+      isFirstPayment: false,
+      idPsp: 'BIC36019',
     }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "ocp-apim-subscription-key": VPOS_API_KEY
-    }
+      'Content-type': 'application/json; charset=UTF-8',
+      'ocp-apim-subscription-key': VPOS_API_KEY,
+    },
   }).then(response => {
     if (response.ok) {
       return response.json();
     } else {
       return {
-        errorHttpStatus: response.status
+        errorHttpStatus: response.status,
       };
     }
   });
